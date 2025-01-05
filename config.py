d5d64a766e614697547a1898920bc9998c89dcf2
SpaciousAbhi <74132323+SpaciousAbhi@users.noreply.github.com>
2025-01-05 20:05:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
+API_ID = int(environ.get("API_ID", "4770590"))
+API_HASH = environ.get("API_HASH", "e33bf9032335b874acb9c6406f044836")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", ""))
-ADMINS = int(environ.get("ADMINS", ""))
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002113810572"))
+ADMINS = int(environ.get("ADMINS", "1654334233"))
 DB_URI = environ.get("DB_URI", "")
 DB_NAME = environ.get("DB_NAME", "vjjoinrequetbot")
