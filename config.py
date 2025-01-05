eb7a24cd0617cef25f49ab9ffd3dff17d461753c
SpaciousAbhi <74132323+SpaciousAbhi@users.noreply.github.com>
2025-01-05 20:11:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ from os import environ
 
 API_ID = int(environ.get("API_ID", "4770590"))
 API_HASH = environ.get("API_HASH", "e33bf9032335b874acb9c6406f044836")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7656809547:AAFbLT6tVyg9AFvbCc2LiOp4xvhElAzfu2g")
 LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002113810572"))
 ADMINS = int(environ.get("ADMINS", "1654334233"))
 DB_URI = environ.get("DB_URI", "")
